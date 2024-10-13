5df2d0cc99d9053b0e486b495dcb04e9a2dd89ac
simpli11ity <51165497+simpli11ity@users.noreply.github.com>
2024-10-13 23:17:50 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 # Specify the AWS provider
 provider "aws" {
-  region = "us-east-1"  # Change to your desired region
+  region = "eu-west-1"  # Change to your desired region
 }
 
 # Define the S3 bucket
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name"  # Change to your desired bucket name
+  bucket = "fefwefwefwefwefw"  # Change to your desired bucket name
   acl    = "private"
 
   tags = {
